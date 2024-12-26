19551304136eee788ce2027d92bc777ac32f1d2b
gopiPatel16 <gopipatel2816@gmail.com>
2024-12-26 13:42:31 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,18 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "tour_travels");
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+    $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    $stmt->bind_param("ss", $username, $password);
+
+    if ($stmt->execute()) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+    $stmt->close();
+}
+$conn->close();
+?>
